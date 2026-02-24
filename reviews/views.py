03f3620e97ebd778a96547a67886482cc7848940
Amit Kumar Das <amit.inverse@gmail.com>
2026-02-24 21:00:28 +0530

    Manual form validation

--- reviews/views.py
+++ reviews/views.py
@@ -6,10 +6,13 @@ from django.http import HttpResponseRedirect
 def review(request):
     if request.method == "POST":
         entered_name = request.POST["your-name"]
+
+        if entered_name == "" or len(entered_name) < 3:
+            return render(request, "reviews/review.html", {"has_error": True})
         print(entered_name)
         return HttpResponseRedirect("/thank-you/")
     
-    return render(request, "reviews/review.html")
+    return render(request, "reviews/review.html", {"has_error": False})
 
 def thank_you(request):
     return render(request, "reviews/thank_you.html")
