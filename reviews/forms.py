d21958b5200247e318ac0dd81ac3a8ed433ba170
Amit Kumar Das <amit.inverse@gmail.com>
2026-02-24 22:39:47 +0530

    Introducing modelforms

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,14 +1,23 @@
 from django import forms
+from reviews.models import Review
 
 
-class ReviewForm(forms.Form):
-    your_name = forms.CharField(label="Your Name", min_length=3, max_length=100, error_messages={
-        "required": "Your name must not be empty",
-        "min_length": "Your name must be at least 3 characters long",
-        "max_length": "Your name must be less than 100 characters long"})
-    
-    review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, min_length=10, max_length=500, error_messages={
-        "required": "Your feedback must not be empty"})
-    
-    rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5, error_messages={
-        "required": "Please provide a rating between 1 and 5"})
+# class ReviewForm(forms.Form):
+#     your_name = forms.CharField(label="Your Name", min_length=3, max_length=100, error_messages={
+#         "required": "Your name must not be empty",
+#         "min_length": "Your name must be at least 3 characters long",
+#         "max_length": "Your name must be less than 100 characters long"})
+
+#     review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, min_length=10, max_length=500, error_messages={
+#         "required": "Your feedback must not be empty"})
+
+#     rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5, error_messages={
+#         "required": "Please provide a rating between 1 and 5"})
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        # fields = "__all__"
+        # exclude = ["owner_comment"]
+        fields = ["your_name", "review_text", "rating"]
