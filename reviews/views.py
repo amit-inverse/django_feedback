064356b635c4c897a9e7408e60141101346fadc5
Amit Kumar Das <amit.inverse@gmail.com>
2026-02-24 20:53:48 +0530

    Handling form submission and extracting data

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def review(request):
+    if request.method == "POST":
+        entered_name = request.POST["your-name"]
+        print(entered_name)
+        return HttpResponseRedirect("/thank-you/")
+    
     return render(request, "reviews/review.html")
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
