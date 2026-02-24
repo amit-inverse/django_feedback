524edc8eb4d3c8acf2b10588ff180fb82b5dddad
Amit Kumar Das <amit.inverse@gmail.com>
2026-02-24 22:48:52 +0530

    Saving data with modelform

--- reviews/views.py
+++ reviews/views.py
@@ -10,12 +10,7 @@ def review(request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
-            review = Review(
-                your_name=form.cleaned_data["your_name"],
-                review_text=form.cleaned_data["review_text"],
-                rating=form.cleaned_data["rating"]
-            )
-            review.save()
+            form.save()
             return HttpResponseRedirect("/thank-you/")
     else:
         form = ReviewForm()
