00a7e9c56addb1136bf1ce57f311897c230b8f11
Amit Kumar Das <amit.inverse@gmail.com>
2026-02-24 21:56:06 +0530

    Adding more form controls

--- reviews/forms.py
+++ reviews/forms.py
@@ -6,3 +6,9 @@ class ReviewForm(forms.Form):
         "required": "Your name must not be empty",
         "min_length": "Your name must be at least 3 characters long",
         "max_length": "Your name must be less than 100 characters long"})
+    
+    review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, min_length=10, max_length=500, error_messages={
+        "required": "Your feedback must not be empty"})
+    
+    rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5, error_messages={
+        "required": "Please provide a rating between 1 and 5"})
