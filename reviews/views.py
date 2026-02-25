57424bd9187db0ded9dfa454e4ab0d9a15b23087
Amit Kumar Das <amit.das@sundewsolutions.com>
2026-02-25 13:13:18 +0530

    FormView

--- reviews/views.py
+++ reviews/views.py
@@ -4,24 +4,29 @@ from django.views import View
 from .forms import ReviewForm
 from .models import Review
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
 from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(request, "reviews/review.html", {"form": form})
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you/")
+    # def post(self, request):
+    #     form = ReviewForm(request.POST)
+
+    #     if form.is_valid():
+    #         form.save()
+    #         return HttpResponseRedirect("/thank-you/")
         
-        return render(request, "reviews/review.html", {"form": form})
+    #     return render(request, "reviews/review.html", {"form": form})
     
 
 class ThankYouView(TemplateView):
